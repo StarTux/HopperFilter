923008cbced89bfd29613f1e2f338c2abc8e386f
Thomas Loy <ths.loy@googlemail.com>
2016-07-09 23:46:43 +0200

    Use exact item

--- src/main/java/com/winthier/hopperfilter/FilterItem.java
+++ src/main/java/com/winthier/hopperfilter/FilterItem.java
@@ -4,28 +4,13 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class FilterItem {
-    private final Material material;
-    private final short data;
-
-    public FilterItem(Material material, short data) {
-        this.material = material;
-        this.data = data;
-    }
+    private final ItemStack itemStack;
 
     public FilterItem(ItemStack item) {
-        this(item.getType(), item.getDurability());
+        this.itemStack = item.clone();
     }
 
     public boolean allowsItem(ItemStack item) {
-        if (material != item.getType()) return false;
-        if (material.getMaxDurability() <= (short)0) return data == item.getDurability();
-        return true;
-    }
-
-    public String toString() {
-        if (material.getMaxDurability() <= (short)0) {
-            return material.name().toLowerCase() + ":" + (int)data;
-        }
-        return material.name().toLowerCase();
+        return itemStack.isSimilar(item);
     }
 }
